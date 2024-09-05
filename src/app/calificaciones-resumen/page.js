1e70eb3ba61509a4820baf6c70424969e07afce5
asanchezudg <angel.sanchezdetagle@udgvirtual.udg.mx>
2024-09-05 15:03:21 -0600

    Se corrige calificaciones resumen

--- src/app/calificaciones-resumen/page.js
+++ src/app/calificaciones-resumen/page.js
@@ -5,15 +5,35 @@ import Link from 'next/link';
 
 const CalificacionesResumen = () => {
   const [resumen, setResumen] = useState({});
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchResumen() {
-      const response = await fetch('/api/calificaciones-resumen');
-      const data = await response.json();
-      setResumen(data);
+      try {
+        const response = await fetch('/api/calificaciones-resumen');
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos');
+        }
+        const data = await response.json();
+        setResumen(data);
+      } catch (error) {
+        setError(error.message);
+      }
     }
     fetchResumen();
   }, []);
+
+  if (error) {
+    return (
+      <div className="container mx-auto p-4 md:max-w-4xl">
+        <h1 className="text-2xl md:text-3xl font-bold mb-4">Error</h1>
+        <p className="text-red-500">{error}</p>
+        <Link href="/" className="mb-4 inline-block bg-blue-500 text-white px-4 py-2 rounded">
+          Volver a la Lista de Tareas
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div className="container mx-auto p-4 md:max-w-4xl">
@@ -23,12 +43,11 @@ const CalificacionesResumen = () => {
       </Link>
       <ul className="mt-4 space-y-4">
         {Object.entries(resumen).map(([materia, { suma, count }]) => (
-            <li key={materia} className="bg-white shadow rounded-lg p-4">
-                <h2 className="text-xl font-semibold">{materia}</h2>
-                <p>Calificacion al momento: {suma.toFixed(2)}</p>
-                {/* <p>Número de tareas calificadas: {count}</p>
-                <p>Promedio: {(suma / count).toFixed(2)}</p> */}
-            </li>
+          <li key={materia} className="bg-white shadow rounded-lg p-4">
+            <h2 className="text-xl font-semibold">{materia}</h2>
+            <p>Calificación al momento: {suma?.toFixed(2)}</p>
+            <p>Número de tareas calificadas: {count}</p>
+          </li>
         ))}
       </ul>
     </div>
